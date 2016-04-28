4179c25d9157d6f4e6ce0d43664b3a5d56a6e6ac
Todd Olsen <todd.olsen@me.com>
2016-04-28 09:50:14 -0400

    problem 25 (overflows)

--- problems/problem_025.swift
+++ problems/problem_025.swift
@@ -6,3 +6,24 @@
 //  Copyright © 2016 Todd Olsen. All rights reserved.
 //
 
+
+// WARNING: This solution overflows. It would give the correct answer otherwise. :)
+func problem_025() -> Int {
+
+    var index = 0
+    var state = (0, 1)
+    let fibs = AnyGenerator<Int> {
+        defer {
+            state = (state.1, state.0 + state.1)
+            index += 1
+        }
+        return state.1
+    }
+
+    while let fib = fibs.next() {
+        if String(fib).characters.count == 1000 { break }
+    }
+
+    return index
+
+}
